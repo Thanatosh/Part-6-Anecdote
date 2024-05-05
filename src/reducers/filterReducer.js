175381fa232bc04ae2bc72a00a7c0fde0d10ad00
Thanatos <Thanatosh@gmail.com>
2024-05-05 10:09:35 +0300

    Part 6.10 Full Commit

--- src/reducers/filterReducer.js
+++ src/reducers/filterReducer.js
@@ -1,17 +1,15 @@
-const filterReducer = (state = null, action) => {
-  switch (action.type) {
-    case 'SET_FILTER':
+import { createSlice } from '@reduxjs/toolkit'
+
+const filterReducer = createSlice({
+  name: 'filter',
+  initialState: null,
+  reducers: {
+    filterChange: (state, action) => {
       return action.payload
-    default:
-      return state
-  }
-}
+    },
+  },
+})
 
-export const filterChange = filter => {
-  return {
-    type: 'SET_FILTER',
-    payload: filter,
-  }
-}
+export const { filterChange } = filterReducer.actions
 
-export default filterReducer
+export default filterReducer.reducer
