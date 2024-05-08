90e946d4cf9516fc53737d2d0d3ae1434ca71802
Thanatos <Thanatosh@gmail.com>
2024-05-08 11:45:54 +0300

    Part 6.14-15 Full Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
+import { useEffect } from 'react'
 import AnecdoteForm from './components/anecdoteForm'
 import AnecdoteList from './components/anecdoteList'
 import Filter from './components/Filter'
 import Notification from './components/Notification'
+import anecdoteService from './services/anecdotes'
+import { setAnecdotes } from './reducers/anecdoteReducer'
+import { useDispatch } from 'react-redux'
 
 const App = () => {
+  const dispatch = useDispatch()
+  useEffect(() => {
+    anecdoteService
+      .getAll().then(anecdotes => dispatch(setAnecdotes(anecdotes)))
+  }, [])
+
   return (
     <div>
       <Notification />
--- src/components/anecdoteForm.jsx
+++ src/components/anecdoteForm.jsx
@@ -1,16 +1,18 @@
 import { useDispatch } from 'react-redux'
 import { addAnecdoteSlice } from '../reducers/anecdoteReducer'
 import { setNotification } from '../reducers/notificationReducer'
+import anecdoteService from '../services/anecdotes'
 
 const newAnecdote = () => {
   const dispatch = useDispatch()
 
-  const addAnecdote = (event) => {
+  const addAnecdote = async (event) => {
     event.preventDefault()
     const content = event.target.elements.anecdote.value
     console.log('content', content)
     event.target.elements.anecdote.value = ''
-    dispatch(addAnecdoteSlice({ content }))
+    const newAnecdote = await anecdoteService.createNew(content)
+    dispatch(addAnecdoteSlice(newAnecdote))
     dispatch(setNotification(`New Anecdote added: ${content}`))
   }
 
